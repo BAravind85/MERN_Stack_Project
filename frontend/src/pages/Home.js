514126f4637c016f5ca2fdf79117851b42408c7e
BAravind85 <102968059+BAravind85@users.noreply.github.com>
2023-01-02 19:00:31 +0530

    homepage blocks

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,55 +1,68 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import Layout from '../Compenents/Layout';
-import { getAllCars } from '../redux/actions/actions';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Layout from "../Compenents/Layout";
+import Loading from "../Compenents/Loading";
+import { getAllCars } from "../redux/actions/actions";
+
 
 const Home = () => {
-    const {cars} = useSelector(state => state.reducer);
-    console.log(cars)
-    const dispatch = useDispatch();
+  const { cars } = useSelector((state) => state.reducer);
+  const {loading}=useSelector((state)=>state.loading);
+  console.log(cars);
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch(getAllCars())
-    },[dispatch]);
+  useEffect(() => {
+    dispatch(getAllCars());
+  }, [dispatch]);
 
   return (
     <Layout>
-        <div className="slider">
-            <div className='left'>
-            <h1 className='title'>TATA NEXON</h1>
-            </div>
-            <div className='right'>
-                <img src='./images/sliders/nexon.png' alt=''/>
-            </div>
+      <div className="slider">
+        <div className="left">
+          <h1 className="title">TATA NEXON</h1>
         </div>
-        <div className='content'>
-            <div className='content-row'>
+        <div className="right">
+          <img src="./images/sliders/nexon.png" alt="" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="content-row">
+          <h1 className="big-title">Top cars for purchase</h1>
+        </div>
+        <div className="content-row">
 
-            </div>
-            <div className='content-row'>
-                <div className='content-groups'>
-                    {cars.map((car)=>{
-                        return <div className='cards'>
-                            <div className='card-body'>
-                                <img src={car.image} alt={car.name}/>
+        </div>
+    
+        <div className="content-row">
+            {loading ? (<Loading/>) : (                
+                <div className="content-groups">
+                    {cars.map((car) => {
+                    return (
+                        <div className="card" key={car._id}>
+                        <div className="card-body">
+                            <img className="img-cars" src={car.image} alt={car.name} />
+                        </div>
+                        <div className="card-footer">
+                            <div className="card-footer-top">
+                            <h3 className="car-title">{car.name}</h3>
+                            <p className="price">
+                                Price{" "}
+                                <span className="bold-price">RS.{car.price}L</span>
+                            </p>
                             </div>
-                            <div className='card-footer'>
-                                <div className='card-footer-top'>
-                                    <h3 className='car-title'>{car.name}</h3>
-                                    <p className='price'>Price RS.{car.price}L</p>
-                                </div>
-                                <div className='card-footer-bottom'>
-                                    <button className='book-now'>Book Now</button> 
-                                </div>
+                            <div className="card-footer-bottom">
+                            <button className="book-now">Book Now</button>
                             </div>
                         </div>
+                        </div>
+                    );
                     })}
                 </div>
-            </div>
-
+            )}
         </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Home;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,29 @@ import Layout from '../Compenents/Layout'
 
 const Login = () => {
   return (
-    <Layout>Login</Layout>
+    <Layout>
+      <div className='form-container'>
+        <div className='form-groups'>
+          <form className='form'>
+            <h3 className='form-title'>Login</h3>
+            <div className='form-group'>
+            <labal htmlFor='username'>Username</labal>
+              <input type='password' id='username' required/>
+            </div>
+            <div className='form-group'>
+              <labal htmlFor='password'>Password</labal>
+              <input type='password' id='password' required/>
+            </div>
+            <div className='form-group'>
+            <button className="book-now">Login</button>
+            </div>
+            <div className='form-group'>
+              <p>Don't have an account ? <a herf='/register'>Here for Register</a> </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
